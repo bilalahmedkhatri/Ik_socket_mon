59149f341a05038fb56c386158f3c357a1ef5c3e
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-02-17 11:05:45 +0500

    .... ?

--- test_chennals/consumers.py
+++ test_chennals/consumers.py
@@ -12,18 +12,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # self.channel_layer.
         await self.accept()
 
     async def receive(self, text_data):
         # Send the image data to the client
-        json_data = json.loads(text_data)
-        print(json_data['frames'])
-        await self.send('send SS from ser dj')
+        json_data = await json.loads(text_data)
+        print("receive: ", json_data)
 
     async def disconnect(self, close_code):
         pass
-
-    async def send_video(self, event):
-        video_data = event['video_data']
-        await self.send(text_data=json.dumps({'video_data': video_data}))
--- test_chennals/routing.py
+++ test_chennals/routing.py
@@ -2,9 +2,9 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'video_data/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r"^ws/(?P<name_of_user>\w+)/$",
-            consumers.ChatConsumer.as_asgi()),
+    re_path(r'user/$', consumers.ChatConsumer.as_asgi()),
+    # re_path(r'ws/$', consumers.ChatConsumer.as_asgi()),
+    # re_path(r"^ws/(?P<name_of_user>\w+)/$",
+    # consumers.ChatsConsumer.as_asgi()),
     # path(r"/", ChatConsumer.as_asgi()),
 ]
