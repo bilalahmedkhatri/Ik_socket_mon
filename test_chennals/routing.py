7de342877aba8ee35b2b5e996614dde211e29546
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-02-21 15:31:42 +0500

    socket.io

--- test_chennals/routing.py
+++ test_chennals/routing.py
@@ -2,13 +2,13 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    # re_path(r'user/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'user/$', consumers.VideoConsumer.as_asgi()),
     # re_path(r'ws/$', consumers.ChatConsumer.as_asgi()),
     # re_path(r"^ws/(?P<name_of_user>\w+)/$",
     # consumers.ChatsConsumer.as_asgi()),
     # path(r"/", ChatConsumer.as_asgi()),
 
     # API DRF handler
-    re_path(r'ws/chat/room/$', consumers.RoomConsumer.as_asgi()),
+    # re_path(r'ws/chat/room/$', consumers.ChatConsumer.as_asgi()),
 
 ]
--- test_chennals/views.py
+++ test_chennals/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def room(request, room_name):
+def video(request, room_name):
     return render(request, "video.html", {"video": room_name})
